bb9f9776288a2deffd5f71933b14775222d3d2f9
garyleung142857 <garyleung142857@gmail.com>
2023-04-15 16:59:57 +0800

    feature: implement header component

---
+++ components/HeaderMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row no-gutters >
+    <v-col cols="4" class="d-flex justify-center">
+      <v-btn
+        text
+      >
+        <v-icon> mdi-information-outline </v-icon> About
+      </v-btn>
+    </v-col>
+    <v-col cols="4" class="d-flex justify-center align-center">
+      {{ appName}}
+    </v-col>
+    <v-col cols="4" class="d-flex justify-center">
+      <v-btn
+        text
+      >
+        <v-icon> mdi-github </v-icon> Source
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu',
+  data () {
+    return {
+      appName: 'Mahjong Waits Test'
+    }
+  },
+}
+</script>

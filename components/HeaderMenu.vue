fd7474a56099c720793b2c5042be922577071cf7
garyleung142857 <garyleung142857@gmail.com>
2023-04-16 17:09:07 +0800

    feature: illustration

--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,20 +1,35 @@
 <template>
-  <v-row no-gutters >
+  <v-row no-gutters>
     <v-col cols="3" class="d-flex justify-center">
-      <v-btn
-        text
-        class="pa-6"
+      <v-dialog
+        v-model="dialog"
+        max-width="600"
       >
-        <v-icon> mdi-information-outline </v-icon>
-      </v-btn>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            text
+            class="pa-6"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon> mdi-information-outline </v-icon>
+          </v-btn>
+        </template>
+        <v-img
+          :src="require('@/assets/illustration.jpg')"
+          class="d-flex align-center"
+        />
+      </v-dialog>
     </v-col>
     <v-col cols="6" class="d-flex justify-center align-center">
-      <h3> {{ appName}} </h3>
+      <h3> {{ appName }} </h3>
     </v-col>
     <v-col cols="3" class="d-flex justify-center">
       <v-btn
         text
         class="pa-6"
+        :href="githubURL"
+        target="_blank"
       >
         <v-icon> mdi-github </v-icon>
       </v-btn>
@@ -27,8 +42,10 @@ export default {
   name: 'HeaderMenu',
   data () {
     return {
-      appName: 'Mahjong Waits Test'
+      dialog: false,
+      appName: 'Mahjong Waits Test',
+      githubURL: 'https://github.com/garyleung142857/mahjong-waits-quiz'
     }
-  },
+  }
 }
 </script>

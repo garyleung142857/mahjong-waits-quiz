3e91a029cba86fc1e4b80b2eeed05ac60a5e937e
garyleung142857 <garyleung142857@gmail.com>
2023-04-17 09:29:45 +0800

    feature: update favicon; update title; update descriptionn

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,19 +9,24 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - mj-one-suit-test',
-    title: 'mj-one-suit-test',
+    titleTemplate: '%s',
+    title: 'Waits Test - 聽牌測驗',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'One-suited wait test' +
+          '清一色聽牌測驗 '
+      },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 
